a5e65bc5debc42164a30ed38a0a63bc5f37392c3
mehdi <mehdi@192.168.0.12>
2019-10-08 13:40:48 +0200

    Slider is now unclickable.

--- src/main/java/views/ElevatorSliderView.java
+++ src/main/java/views/ElevatorSliderView.java
@@ -8,6 +8,8 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.plaf.SliderUI;
 import java.awt.*;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -18,12 +20,14 @@ public class ElevatorSliderView extends JFrame implements Observer {
 
     public ElevatorSliderView(int steps, ElevatorSystem system) {
         this.slider = new JSlider(JSlider.VERTICAL, 0, steps, 0);
+        for (MouseListener listener : slider.getMouseListeners())
+        	slider.removeMouseListener(listener);
+
 
         this.elevatorSystem = system;
         this.elevatorSystem.addObserver(this);
 
         this.add(this.slider);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.slider.addChangeListener(new ChangeListener() {
